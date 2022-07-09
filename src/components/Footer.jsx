c8214149e5720e0ab07fb408090b64b4feb73f19
birkan <b.561birkan@gmail.com>
2022-07-09 17:19:29 +0300

    fetching data from api, taking some parts of data

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="page-footer">
+      <div className="container">
+        <small>Created in {new Date().getFullYear()}🎉 </small>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
+import WeatherCard from "./WeatherCard";
+
 const Main = () => {
+  const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState([]);
+  const handleChange = (e) => {
+    // console.log(e.target.value);
+    setSearchText(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeatherDataFromApi();
+    setSearchText("");
+  };
+  const getWeatherDataFromApi = async () => {
+    let apiKey = process.env.REACT_APP_API_KEY;
+    let units = "metric";
+    let lang = "tr";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=${apiKey}&units=${units}&lang=${lang}`;
+    try {
+      const response = await axios.get(url);
+      // console.log(response);
+      const { main, name, sys, weather, id } = response.data;
+      const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+      setData([{ main, name, sys, weather, iconUrl, id }]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(data);
   return (
     <section className="main">
-      <form>
-        <input type="text" placeholder="Search for a city" autoFocus />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search for a city"
+          autoFocus
+          onChange={handleChange}
+          value={searchText}
+        />
         <button type="submit">SUBMIT</button>
         <span className="msg">aaaaa</span>
       </form>
       <div className="container">
-        <ul className="cities">Main</ul>
+        <ul className="cities">
+          <WeatherCard />
+        </ul>
       </div>
     </section>
   );
