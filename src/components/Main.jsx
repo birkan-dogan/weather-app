0934a5d3f46b26d98e524b3f24c5d5d77c8e85b8
birkan <b.561birkan@gmail.com>
2022-07-09 14:57:48 +0300

    adding Main comp. and styling not included logic

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <section className="main">
+      <form>
+        <input type="text" placeholder="Search for a city" autoFocus />
+        <button type="submit">SUBMIT</button>
+        <span className="msg">aaaaa</span>
+      </form>
+      <div className="container">
+        <ul className="cities">Main</ul>
+      </div>
+    </section>
+  );
+};
+export default Main;
