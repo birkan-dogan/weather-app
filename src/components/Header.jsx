eeee2b9648df33e3c536d520daa1e64310e8cd39
birkan <b.561birkan@gmail.com>
2022-07-09 14:28:02 +0300

    adding Header component and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,15 @@
+import image from "../images/weather.png";
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <section className="top-banner">
+      <div className="container">
+        <center>
+          <img src={image} alt="weather" />
+          <h1 className="heading">Weather App</h1>
+        </center>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
